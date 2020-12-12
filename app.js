06b9e0fb02c6903277794380fc3d80edf6f0ebea
tianrui.zhang <tianrui.zhang@ly.com>
2020-12-12 15:44:36 +0800

    add: 后端添加跨域

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ import singer from './routes/singer'
 
 const app = new Koa()
 
+// 设置跨域 
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200; 
+  } else {
+    await next();
+  }
+});
+
 app.use(koaBody({multipart: true}))
 app.use(singer.routes())
 
